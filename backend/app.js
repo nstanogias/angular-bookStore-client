a27d4a4eb9fcfd71f79fccf847b90972e2972cc6
Nikos Stanogias <nstanogias@gmail.com>
2019-09-24 21:35:59 +0200

    preparing for deploy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const booksRoutes = require("./routes/books");
 const userRoutes = require("./routes/user");
@@ -10,7 +11,7 @@ const app = express();
 
 mongoose
   .connect(
-    "",
+    "process.env.MONGO_URI",
     { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true }
   )
   .then(() => {
@@ -22,22 +23,26 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//   );
+//   next();
+// });
 
 app.use("/api/books", booksRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/orders", ordersRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
